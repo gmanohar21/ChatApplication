8ea88581613098d928cda6c7458bffa51d8ae9e5
gmanohar21 <gmanohar21@gmail.com>
2023-07-26 00:33:45 +0530

    Project Commit

---
+++ ChatApplicationSocketApp/src/main/java/com/mn/ChatApplicationSocketAppApplication.java
@@ -0,0 +1,13 @@
+package com.mn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatApplicationSocketAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatApplicationSocketAppApplication.class, args);
+	}
+
+}
